1bf685af0f7c2e24645a36db059158f02313b8ec
VJean <admin@vjean.net>
2016-08-29 13:00:12 +0300

    Translate update

---
+++ translations/update_ru.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>TeamSpeak 3 Client Update</source>
+        <translation>Обновление клиента TeamSpeak 3</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Подробности</translation>
+    </message>
+    <message>
+        <source>Stable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Beta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alpha</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update to TeamSpeak %1</source>
+        <translation>Обновление для TeamSpeak %1</translation>
+    </message>
+    <message>
+        <source>You are currently on &lt;strong&gt;%1 channel&lt;/strong&gt;.</source>
+        <translation>Вы используете &lt;strong&gt;%1 канал&lt;/strong&gt;.</translation>
+    </message>
+    <message>
+        <source>Step one: Updating updater</source>
+        <translation>Шаг 1: Обновление инсталятора</translation>
+    </message>
+    <message>
+        <source>Step two: Updating TeamSpeak client</source>
+        <translation>Шаг 2: обновление клиента TeamSpeak</translation>
+    </message>
+    <message>
+        <source>Confirm reboot</source>
+        <translation>Подтверждение перезагрузки</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;A computer restart is required to proceed with the client update. If you do not reboot now, the TeamSpeak update will be cancelled.&lt;/p&gt;&lt;p&gt;Do you want to reboot now?&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Требутеся перезагрузка компьютера для продолжения обновления клиента. Если Вы откажетесь от перезагрузки, обновление TeamSpeak будет отменено.&lt;/p&gt;&lt;p&gt;Перезагрузить сейчас?&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>Вопрос</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Update in progress.&lt;/p&gt;&lt;p&gt;Are you sure you want to abort?&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Выполняется обновление.&lt;/p&gt;&lt;p&gt;Вы уверены, что хотите отменить?&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Waiting until TeamSpeak 3 Client has been closed.</source>
+        <translation>Подождите, пока TeamSpeak 3 клиент будет закрыт.</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <source>The TeamSpeak 3 Client should be
+closed before starting the update.</source>
+        <translation>TeamSpeak 3 клиент должен быть 
+закрыт перед началом обновления.</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;This is the last TeamSpeak version available for Windows XP.&lt;/p&gt;&lt;p&gt;To upgrade TeamSpeak and take advantage of the latest features and bug fixes, please update your operating system to Windows Vista or newer.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Это последняя версия клиента TeamSpeak3, доступная для Windows XP.&lt;/p&gt;&lt;p&gt;Чтобы получить доступ к более современным версиям клиента TeamSpeak3 с новыми функциями и исправленными ошибками, обновите свою операционную систему до Windows Vista или более новой.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>%1 KB / %2 KB</source>
+        <translation>%1 КБ / %2 КБ</translation>
+    </message>
+    <message>
+        <source>%1 KB/s</source>
+        <translation>%1 КБ/с</translation>
+    </message>
+    <message>
+        <source>Update cancelled</source>
+        <translation>Обновление отменено</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Обновить</translation>
+    </message>
+    <message>
+        <source>Update cancelled. Push &quot;Details&quot; to check reasons.</source>
+        <translation>Обновление отменено. Нажмите &quot;Подробности&quot;, чтобы узнать причины.</translation>
+    </message>
+    <message>
+        <source>Click to open &quot;%1://%2&quot;</source>
+        <translation>Нажмите, чтобы открыть &quot;%1://%2&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished">Ошибка</translation>
+    </message>
+    <message>
+        <source>Failed to query latest client
+version from update server.</source>
+        <translation type="unfinished">Не удалось запросить последную версию 
+клиента на сервере обновлений.</translation>
+    </message>
+    <message>
+        <source>Update server returned no version</source>
+        <translation type="unfinished">Сервер обновлений возратил отсутствие версий</translation>
+    </message>
+    <message>
+        <source>Update server returned invalid version nr (%1)</source>
+        <translation type="unfinished">Сервер обновления вернул неверную версию номер (%1)</translation>
+    </message>
+    <message>
+        <source>Invalid version nr (%1)</source>
+        <translation type="unfinished">Неверная версия номер (%1)</translation>
+    </message>
+</context>
+</TS>
