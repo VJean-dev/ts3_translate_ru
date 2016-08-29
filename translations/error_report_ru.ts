dce24006fdd0cdf37d87ed984e93c41cf5e754a5
VJean <admin@vjean.net>
2016-08-29 12:52:00 +0300

    Translate error_report

--- translations/error_report_ru.ts
+++ translations/error_report_ru.ts
@@ -1,29 +1,31 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>ErrorReport</name>
     <message>
         <source>TeamSpeak 3 Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка TeamSpeak 3</translation>
     </message>
     <message>
         <source>Do you want to send the error report?</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы хотите выслать отчет об ошибке?</translation>
     </message>
     <message>
         <source>The TeamSpeak 3 client has crashed. There is a debug file which will help
 the developers to fix the problem. Please type a description in the text
 field to reproduce the issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Клиент TeamSpeak 3 завершился с критической ошибкой. 
+Вы можете помочь разработчикам, отправив отчет о сбое. 
+Пожалуйста, напишите как воспроизвести ошибку. </translation>
     </message>
     <message>
         <source>Thank you for your report!</source>
-        <translation type="unfinished"></translation>
+        <translation>Спасибо за ваш отчет!</translation>
     </message>
     <message>
         <source>An error occured while transmitting the data.</source>
-        <translation type="unfinished"></translation>
+        <translation>Произошла ошибка при отправке данных.</translation>
     </message>
 </context>
 </TS>
