fa9f3a903f6c50305e6a12ca826a57f7284525da
VJean <admin@vjean.net>
2016-08-29 12:57:09 +0300

    Translate package_installer

---
+++ translations/package_installer_ru.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>InstallDialog</name>
+    <message>
+        <source>TeamSpeak Add-On</source>
+        <translation>Дополнение TeamSpeak</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Название:</translation>
+    </message>
+    <message>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <source>Author:</source>
+        <translation>Автор:</translation>
+    </message>
+    <message>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <source>Platform:</source>
+        <translation>Платформа:</translation>
+    </message>
+    <message>
+        <source>This add-on is not available for your platform.</source>
+        <translation>Это дополнение не доступно для Вашей платформы.</translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation>Установить</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <source>Failed to read Add-On info.</source>
+        <translation>Не удалось прочитать информацию о дополнении.</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Все</translation>
+    </message>
+    <message>
+        <source>Select TeamSpeak Client installation folder</source>
+        <translation>Выберите папку установки клиента TeamSpeak</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;You are about to install a Plugin for your TeamSpeak Client.&lt;/p&gt;&lt;p&gt;Plugins should only be installed from trusted sources, as it is possible to inject malicious code into the TeamSpeak client with a plugin.&lt;/p&gt;&lt;p&gt;Do you trust this Plugin and want to proceed installing?&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Вы собираетесь установить плагин для клиента TeamSpeak3.&lt;/p&gt;&lt;p&gt;Плагины должны быть установлены только из надежных источников, иначе так можно внедрить вредоносный код в клиента TeamSpeak3.&lt;/p&gt;&lt;p&gt;Доверяете ли Вы этому плагину и хотите продолжить установку?&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>No TeamSpeak 3 Client installation directory found.</source>
+        <translation>Не найден каталог установки клиента TeamSpeak 3.</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Add-On successfully installed.&lt;br&gt;Do you want to activate this Add-On?&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Please restart your TeamSpeak Client.&lt;/strong&gt;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Дополнение успешно установлено. &lt;br&gt;Вы хотите активировать это дополнение?&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Пожалуйста, перезапустите клиент TeamSpeak.&lt;/strong&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Add-On successfully installed.</source>
+        <translation>Дополнение успешно установлено.</translation>
+    </message>
+    <message>
+        <source>Failed to install Add-On.
+Do you want to retry as Administrator?</source>
+        <translation>Не удалось установить дополнение.
+Вы хотите, чтобы повторить с правами Администратора?</translation>
+    </message>
+    <message>
+        <source>Failed to install Add-On.</source>
+        <translation>Не удалось установить дополнение.</translation>
+    </message>
+    <message>
+        <source>File not found: %1</source>
+        <translation>Файл не найден: %1</translation>
+    </message>
+    <message>
+        <source>Invalid zip file: %1</source>
+        <translation>Неправильный zip файл: %1</translation>
+    </message>
+    <message>
+        <source>Failed to find temp directory</source>
+        <translation>Не удалось найти временную директорию</translation>
+    </message>
+    <message>
+        <source>Failed to create temp path: %1</source>
+        <translation>Не удалось создать временный путь: %1</translation>
+    </message>
+    <message>
+        <source>Failed to extract: %1</source>
+        <translation>Не удалось извлечь:%1</translation>
+    </message>
+    <message>
+        <source>Failed to extract Add-On:
+%1</source>
+        <translation>Не удалось извлечь дополнение:
+%1</translation>
+    </message>
+</context>
+</TS>
